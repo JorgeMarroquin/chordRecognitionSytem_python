2604171cb21be5e2ede104aabffae48e0b522bd9
JorgeMarroquín <Jorge.MO@alliedglobal.com>
2022-09-01 08:32:37 -0600

    fix readme

--- main.py
+++ main.py
@@ -31,13 +31,12 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model.fit(train_dataset, steps_per_epoch=3, epochs=10, validation_data=test_dataset)
+history = model.fit(train_dataset, steps_per_epoch=3, epochs=50, validation_data=test_dataset)
 
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
 
 plt.show()
